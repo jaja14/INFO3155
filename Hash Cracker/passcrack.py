504d5d49469878b119c8cf08f4d0c67a099dfac4
Javaunne <javaunne4@gmail.com>
2015-02-10 23:39:22 -0500

    Updated Files

--- Hash Cracker/passcrack.py
+++ Hash Cracker/passcrack.py
@@ -2,12 +2,12 @@ import crypt
 import time
 
 def testpass(uname,cPass,dname):
-	salt = cPass[0:2]
+	salt = cPass[0:2] #this extracts the salt from the hash...(the salt is the first two characters
 	dfile = open(dname,"r")
 	for word in dfile.readlines():
 		word = word.strip("\n")
-		cword = crypt.crypt(word,salt)
-		if (cword == cPass):
+		cword = crypt.crypt(word,salt) #this is encrypting the cleartext word and storing the cypher as cword
+		if (cword == cPass): #this tests to see if the newly encrypted password is equal to the user's password hash
 			print "Password for " +uname +" is " +word +"\n"
 			return
 	print "No password found for " +uname +"\n"
